b286f3934c4b47109505762ad83ea6a5d75c9b09
Jozo <jozo@example.com>
2023-07-11 13:02:48 +0200

    Call NYT Movie API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,9 @@
             :key="n"
             cols="4"
           >
-            <v-card height="200"></v-card>
+            <v-card height="200">
+            {{test}}
+            </v-card>
           </v-col>
         </v-row>
       </v-container>
@@ -18,6 +20,36 @@
   export default {
     name: 'HomeView',
 
+data() {
+  return {
+    test: 'dsdsd',
+    Movie : []
+  }
+},
+
+
+
+created(){
+console.log('created')
+this.getData();
+},
+
+
+methods: {
+  getData() {
+    let api ="https://api.nytimes.com/svc/movies/v2/critics/{reviewer}.json"
     
+    this.axios.get(api, {
+      params: {
+        'api-key': 'MOmOdyMp2WeAFbW5BHGWlCzoTPHXIZLA'
+      }
+    }).then((response) => {
+  console.log(response.data)
+  this.Movie = response.data.resoults
+})
+  }
+}
+
+
   }
 </script>
