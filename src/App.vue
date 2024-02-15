058141b8960d4f373b97d341d6a97275f906cc3b
Jozo <jozo@example.com>
2024-02-15 19:36:08 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,145 @@
 <template>
-    <v-app id="inspire">
-    <v-app-bar
-      app
-      extended
-    >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Automobili</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-     <router-view/>
-    </v-main>
-  </v-app>
-
+  <v-container>
+  <v-row>
+  <v-col cols="12">
+   <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search by title"
+        single-line
+        hide-details
+        :loading="isLoading"
+        
+      ></v-text-field>
+  </v-col>
+  <v-col cols="12 ">
+   <v-text-field
+        v-model="author"
+        append-icon="mdi-magnify"
+        label="Search by author"
+        single-line
+        hide-details
+        :loading="isLoadingAuthor"
+        
+      ></v-text-field>
+  </v-col>
+  </v-row>
+        <v-row>
+          <v-col
+            v-for="book in books"
+            :key="book.title"
+            cols="4"
+            md="3"
+          >
+            <v-card height="200">
+            <h3>{{book.title}}</h3>
+            <h5>{{book.author}}</h5>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-col cols="12">
+          <v-pagination
+      v-model="page"
+      :length="Math.ceil(totalBooks/perPage)"
+      :total-visible="7"
+    ></v-pagination>
+        </v-col>
+        <v-row> 
+   
+        </v-row>
+      </v-container> 
 </template>
 
 <script>
+  
 
-export default {
-  name: 'App',
+  export default {
+    name: 'HomeView' ,
+   
 
-  data: () => ({
-    //
-  }),
-};
+ data() {
+  return {
+    test: 'Testna varijabla',
+    books : [],
+    page: 1,
+    totalBooks: 0,
+    perPage: 20,
+    search: '',
+    author: '',
+    isLoading: false,
+    isLoadingAuthor: false
+  }
+  },
+
+
+
+ created(){
+ console.log('created')
+  this.getData();
+ },
+
+
+ methods: {
+  getData() {
+    let api ="https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json"
+    
+    this.axios.get(api, {
+      params: {
+        'api-key': 'MOmOdyMp2WeAFbW5BHGWlCzoTPHXIZLA',
+        'offset': this.perPage * (this.page -1),
+        'title': this.search,
+        'author':this.author
+      }
+    }).then((response) => {
+  console.log(response.data)
+  this.books = response.data.results
+  this.totalBooks = response.data.num_results
+  this.isLoading = false
+   })
+   },
+
+
+
+  
+     fetchEntriesDebounced() {
+      clearTimeout(this._searchTimerId)
+      this._searchTimerId = setTimeout(() => {
+        this.getData()
+      }, 700) /* 500ms throttle */
+    
+   },
+   searchEntries() {
+      this.books = []
+      this.page = 1
+      this.fetchEntriesDebounced()
+    }
+  },
+
+  watch: {
+   page:function() {
+    this.getData();
+    },
+    search (val) {
+      if (!val) {
+        return
+      }
+      this.isLoading = true
+      this.searchEntries()
+     },
+    author (val) {
+      if (!val) {
+        return
+      }
+      this.isLoadingAuthor = true
+      this.searchEntries();
+    }
+  }
+}
+
+
+ 
+ 
+  
+ 
 </script>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,55 +1,141 @@
 <template>
   <v-container>
+  <v-row>
+  <v-col cols="12">
+   <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search by title"
+        single-line
+        hide-details
+        :loading="isLoading"
+        
+      ></v-text-field>
+  </v-col>
+  <v-col cols="6">
+   <v-text-field
+        v-model="author"
+        append-icon="mdi-magnify"
+        label="Search by author"
+        single-line
+        hide-details
+        :loading="isLoadingAuthor"
+        
+      ></v-text-field>
+  </v-col>
+  </v-row>
         <v-row>
           <v-col
-            v-for="n in 24"
-            :key="n"
+            v-for="book in books"
+            :key="book.title"
             cols="4"
+            md="3"
           >
-            <v-card height="200">
-            {{test}}
+       <v-card height="200">
+            <h3>{{book.title}}</h3>
+            <h5>{{book.author}}</h5>
             </v-card>
+
           </v-col>
+        </v-row>
+        <v-col cols="12">
+          <v-pagination
+      v-model="page"
+      :length="Math.ceil(totalBooks/perPage)"
+      :total-visible="7"
+    ></v-pagination>
+        </v-col>
+        <v-row> 
+   
         </v-row>
       </v-container>
 </template>
 
 <script>
   
-
   export default {
-    name: 'HomeView',
+    name: 'HomeView' ,
 
-data() {
+    
+ data() {
   return {
-    test: 'dsdsd',
-    Movie : []
+    test: 'Testna varijabla',
+    books : [],
+    page: 1,
+    totalBooks: 0,
+    perPage: 20,
+    search: '',
+    author: '',
+    isLoading: false,
+    isLoadingAuthor: false
   }
-},
+  },
 
 
 
-created(){
-console.log('created')
-this.getData();
-},
+ created(){
+ console.log('created')
+  this.getData();
+ },
 
-
-methods: {
+ methods: {
   getData() {
-    let api ="https://api.nytimes.com/svc/movies/v2/critics/{reviewer}.json"
+    let api ="https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json"
     
     this.axios.get(api, {
       params: {
-        'api-key': 'MOmOdyMp2WeAFbW5BHGWlCzoTPHXIZLA'
+        'api-key': 'MOmOdyMp2WeAFbW5BHGWlCzoTPHXIZLA',
+        'offset': this.perPage * (this.page -1),
+        'title': this.search,
+        'author':this.author
       }
     }).then((response) => {
   console.log(response.data)
-  this.Movie = response.data.resoults
-})
+  this.books = response.data.results
+  this.totalBooks = response.data.num_results
+  this.isLoading = false
+   })
+   },
+
+
+
+  
+     fetchEntriesDebounced() {
+      clearTimeout(this._searchTimerId)
+      this._searchTimerId = setTimeout(() => {
+        this.getData()
+      }, 700) /* 500ms throttle */
+    
+   },
+   searchEntries() {
+      this.books = []
+      this.page = 1
+      this.fetchEntriesDebounced()
+   }
+  },
+
+ watch:{
+   page:function() {  
+    this.getData();
+    },
+    search (val) {
+      if (!val) {
+        return
+      }
+      this.isLoading = true
+      this.searchEntries()
+     },
+
+    author (val) {
+      if (!val) {
+        return
+      }
+      this.isLoadingAuthor = true
+      this.searchEntries();
+    }
   }
 }
+  
 
-
-  }
 </script>
+
